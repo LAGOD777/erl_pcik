04c4eac91b4b6d14cd9b3c1f5ba9c9cd18f4f08d
张晓泉 <zhang_xiao_quan@qq.com>
2014-10-24 17:23:18 +0800

    num_sup.erl

---
+++ num_sup.erl
@@ -0,0 +1,41 @@
+-module(num_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+	%启动数据库
+	start_mysql(),
+	% inets:start(),
+	% zlib:open(),
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+	Loopnum = ?CHILD(loopnum,worker),
+	% Simple = ?CHILD(simple_sup,supervisor),
+    {ok, { {one_for_one, 5, 10}, [Loopnum]} }.
+
+
+%数据库连接
+start_mysql() ->
+	crypto:start(),
+	application:start(emysql),
+	emysql:add_pool(pool,1,
+		"numberdb","dbzhidaolezcx","misszhucx.mysql.rds.aliyuncs.com",
+			3306,"callnumber",utf8).
